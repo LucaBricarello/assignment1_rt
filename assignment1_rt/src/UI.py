485f32a9806e42451d51b787b550c2d713c5acfc
Luca Bricarello <luca.bricarello@outlook.it>
2024-11-11 23:44:49 +0000

    Added functionalities to UI.py, implemented textual interface and assignment of the decided velocity to the desired turtle

--- assignment1_rt/src/UI.py
+++ assignment1_rt/src/UI.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 
+
+
 import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
@@ -10,33 +12,83 @@ from turtlesim.srv import Spawn
 
 
 
-def turtle_callback(msg):
+def turtle_callback():
+    
+	print("\n")
+	print("Which turtle do you want to move? (insert 1 or 2)\n")
+	turtle_decision = int(input("--> "))
+	print("\n")
+    
+	print("To go ahead insert 1 \nTo rotate insert 2 \nTo translate left or right insert 3\n")
+	movement_decision = int(input("--> "))
+	print("\n")
+    
+	print("Choose velocity intensity\n")
+	intensity_decision = float(input("--> "))
+	print("\n")
 
-    rospy.loginfo("Turtle subscriber@[%f, %f, %f]", msg.x, msg.y, msg.theta)
+	# ------------- building my_vel msg
 
-    rate.sleep()
+	if movement_decision == 1 :
+    		my_vel.linear.x = intensity_decision
+    		my_vel.linear.y = 0
+    		my_vel.angular.z = 0
+    	
+	elif movement_decision == 2 :
+		my_vel.linear.x = 0
+		my_vel.linear.y = 0
+		my_vel.angular.z = intensity_decision
+		
+	elif movement_decision == 3 :
+		my_vel.linear.x = 0
+		my_vel.linear.y = intensity_decision
+		my_vel.angular.z = 0
+    	
+	else :
+		print("ERROR. movement code invalid")
+		my_vel.linear.x = 0
+		my_vel.angular.z = 0
+
+	# ----------- sending my_vel msg to the right turtle
+
+	if turtle_decision == 1 :
+
+		pub1.publish(my_vel)
+    
+	elif turtle_decision == 2 :
+        
+		pub2.publish(my_vel)
+    
+	else :
+		print("ERROR. only turtle1 and turtle2 are available, invalid input")
+    
+	rate.sleep()
 
 
 
 
 def main():
-    global pub, rate
+    global pub1, pub2, rate, my_vel
     
     # Initialize the node
     rospy.init_node('Assignment_UI', anonymous=True)
 
     # Define the publisher and subscriber
-    pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=1)
-    rospy.Subscriber('turtle1/pose', Pose, turtle_callback)
+    pub1 = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=1)
+    pub2 = rospy.Publisher('turtle2/cmd_vel', Twist, queue_size=1)
     
     # Define the service client and call it to spawn turtle2
     client1_spawn = rospy.ServiceProxy('/spawn', Spawn)
     client1_spawn(4.0, 4.0, 0.0, "turtle2")
     
+    my_vel = Twist()
+    
     rate = rospy.Rate(1)
+    
+    while not rospy.is_shutdown() :
+    	turtle_callback()
 
-    rospy.spin()
-
+    #rospy.spin()
 
 
 
