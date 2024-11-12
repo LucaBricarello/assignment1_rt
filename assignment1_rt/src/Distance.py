ab0ae4d1eed3050f943c8a8813c2ac39667d070a
Luca Bricarello <luca.bricarello@outlook.it>
2024-11-12 23:00:42 +0000

    Modified Distance.py, added functionality that stops the turtle if too close to the border or to the other turtle

--- assignment1_rt/src/Distance.py
+++ assignment1_rt/src/Distance.py
@@ -9,11 +9,22 @@ import math
 # -----------------------------------------------------------------------------------------------
 
 def turtle1_callback(msg):
-	global turtle1_x, turtle1_y
+	global turtle1_x, turtle1_y, my_vel
 
 	# Saving updated position in global variables
 	turtle1_x = msg.x
 	turtle1_y = msg.y
+	
+	if msg.x > 10 or msg.x < 1 or msg.y > 10 or msg.y < 1 :
+		# Generate stopping msg
+		my_vel.linear.x = 0
+		my_vel.linear.y = 0
+		my_vel.angular.z = 0
+		
+		print("WARNING: turtle1 too close to the border, stopping it")
+        	
+		# publish stopping msg to both turtle since I don't know wich one is moving
+		pub1.publish(my_vel)
 	
 	# Calling the function that computes the distance and publishes it
 	compute_distance()	
@@ -21,11 +32,22 @@ def turtle1_callback(msg):
 # -----------------------------------------------------------------------------------------------
 	
 def turtle2_callback(msg):
-	global turtle2_x, turtle2_y
+	global turtle2_x, turtle2_y, my_vel
 
 	# Saving updated position in global variables
 	turtle2_x = msg.x
 	turtle2_y = msg.y
+	
+	if msg.x > 10 or msg.x < 1 or msg.y > 10 or msg.y < 1 :
+		# Generate stopping msg
+		my_vel.linear.x = 0
+		my_vel.linear.y = 0
+		my_vel.angular.z = 0
+		
+		print("WARNING: turtle2 too close to the border, stopping it")
+        	
+		# publish stopping msg to both turtle since I don't know wich one is moving
+		pub2.publish(my_vel)
 	
 	# Calling the function that computes the distance and publishes it
 	compute_distance()	
@@ -33,7 +55,7 @@ def turtle2_callback(msg):
 # -----------------------------------------------------------------------------------------------
 
 def compute_distance() :
-	global turtle1_x, turtle1_y, turtle2_x, turtle2_y, pub_dist
+	global turtle1_x, turtle1_y, turtle2_x, turtle2_y, pub_dist, my_vel
 	
 	# Computing the distance
 	distance = math.sqrt(pow(turtle1_x - turtle2_x, 2) + pow(turtle1_y - turtle2_y, 2))
@@ -45,9 +67,21 @@ def compute_distance() :
         # Publish the msg
 	pub_dist.publish(distance_msg)
 	# To check if the msg is sent correctly run the following command on a fresh terminal to listen to the topic: rostopic echo /turtle_distance
+	
+	threshold = 1
         
-        # Debug
-	rospy.loginfo(f"Published distance: {distance}\n")
+	if (distance <= threshold) and (stopFlag == 0)  :
+		# Generate stopping msg
+		my_vel.linear.x = 0
+		my_vel.linear.y = 0
+		my_vel.angular.z = 0
+		
+		print("WARNING: turtles too close to each other, stopping them")
+        	
+		# publish stopping msg to both turtle since I don't know wich one is moving
+		pub1.publish(my_vel)
+		pub2.publish(my_vel)
+		
 
 # -----------------------------------------------------------------------------------------------
 
@@ -64,7 +98,7 @@ def main():
 	rospy.Subscriber('/turtle1/pose', Pose, turtle1_callback)
 	rospy.Subscriber('/turtle2/pose', Pose, turtle2_callback)
 
-	#my_vel = Twist()
+	my_vel = Twist()
 	
 	# Setting up variables so that the first loop doesn't give warnings and problems
 	turtle1_x = 5.54
